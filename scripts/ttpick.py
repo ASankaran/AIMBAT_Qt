2724917ece487dda47af6eb34a1946eef822a7d7
ASankaran <arnavsankaran@gmail.com>
2016-07-29 09:12:27 -0500

    Added iccs and mccc scripts

--- scripts/ttpick.py
+++ scripts/ttpick.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+#------------------------------------------------
+# Filename: ttpick.py
+#   Author: Arnav Sankaran
+#    Email: arnavsankaran@gmail.com
+#
+# Copyright (c) 2016 Arnav Sankaran
+#------------------------------------------------
 from pysmo.aimbat_qt.GUI import mainGUI
 from pysmo.aimbat_qt.seismodata import getDataOpts
 
